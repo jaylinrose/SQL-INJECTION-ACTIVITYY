5d0f2a0347a2d03f4053c6ffe318a9d78dfc2709
jaylinrose <jaylin.manuel02@gmail.com>
2024-03-04 18:08:47 -0800

    put on port 7000 and tested login

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ express = require('express'),
 bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
+const port = 3000;
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 const db = new sqlite3.Database(':memory:');
-
 db.serialize(function () {
  db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
  db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
 });
+
 
 //GET ROUTE
 
@@ -33,7 +34,7 @@ app.post('/login', function (req, res) {
     console.log("password: " + password);
     console.log("query: " + query);
 
- db.get(query, function (err, row) {
+ 	db.get(query, function (err, row) {
 
 		if (err) {
 			console.log('ERROR', err);
@@ -44,11 +45,11 @@ app.post('/login', function (req, res) {
 			res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>');
 		}
 	});
-    
-    res.sendFile('index.html');
 });
 
-app.listen(7000)
+app.listen(3000, function () {
+	console.log("Express server listening on port 3000");
+	});
 
 
 
